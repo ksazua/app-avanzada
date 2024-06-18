5e3124fb7c8c27379f17f197344a2a1660676ad9
ksazua <ksazua@espe.edu.ec>
2024-06-17 21:54:45 -0500

    Subida del componente

--- ProyectoAvanzadaU1/src/app/app.module.ts
+++ ProyectoAvanzadaU1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetAdoptionComponent } from './pet-adoption/pet-adoption.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetAdoptionComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoAvanzadaU1/src/app/pet-adoption/pet-adoption.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-adoption',
+  templateUrl: './pet-adoption.component.html',
+  styleUrl: './pet-adoption.component.css'
+})
+export class PetAdoptionComponent {
+
+}
