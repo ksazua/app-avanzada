b4cbcd05949d9ef06ef3d6b0cbc31c0d14340e43
ejespinoza5 <ejespinoza5@espe.edu.ec>
2024-06-17 22:02:47 -0500

    crear componente

--- ProyectoAvanzadaU1/src/app/app.module.ts
+++ ProyectoAvanzadaU1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetAdoptionComponent } from './Componentes/pet-adoption/pet-adoption.component';
+import { TablaValidacionComprobanteComponent } from './tabla-validacion-comprobante/tabla-validacion-comprobante.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetAdoptionComponent
+    PetAdoptionComponent,
+    TablaValidacionComprobanteComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoAvanzadaU1/src/app/tabla-validacion-comprobante/tabla-validacion-comprobante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-validacion-comprobante',
+  templateUrl: './tabla-validacion-comprobante.component.html',
+  styleUrl: './tabla-validacion-comprobante.component.css'
+})
+export class TablaValidacionComprobanteComponent {
+
+}
