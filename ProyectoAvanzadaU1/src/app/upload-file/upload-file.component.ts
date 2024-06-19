7df49b7e6ac432fc2e86ee239413ba80cae8535d
FabricioAlama2001 <88296370+FabricioAlama2001@users.noreply.github.com>
2024-06-18 22:14:19 -0500

    update upload

--- ProyectoAvanzadaU1/src/app/upload-file/upload-file.component.ts
+++ ProyectoAvanzadaU1/src/app/upload-file/upload-file.component.ts
@@ -10,6 +10,8 @@ import { format } from 'date-fns';
 })
 export class UploadFileComponent {
   form: FormGroup;
+  file: File | null = null;
+  isSubmitted = false;
 
   constructor(private messageService: MessageService, private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -22,18 +24,31 @@ export class UploadFileComponent {
   }
 
   onSelect(event: any) {
-    this.messageService.add({ severity: 'info', summary: 'Revisión', detail: 'El archivo está en proceso de revisión.' });
-    for (let file of event.files) {
-      this.simulateUpload(file);
+    if (this.file) {
+      this.messageService.add({ severity: 'warn', summary: 'Advertencia', detail: 'Ya se ha seleccionado un archivo. Elimine el archivo actual para seleccionar uno nuevo.' });
+      return;
     }
+    this.file = event.files[0];
+    this.isSubmitted = false;
   }
 
-  simulateUpload(file: File) {
-    setTimeout(() => {
-      const now = new Date();
-      const formattedDate = format(now, 'yyyy-MM-dd HH:mm:ss');
-      console.log('Archivo subido:', file.name);
-      this.messageService.add({ severity: 'success', summary: 'Éxito', detail: `El archivo ha sido subido con éxito a las ${formattedDate}.` });
-    }, 3000); // Simula un retraso en la subida de 3 segundos
+  onRemove(event: any) {
+    this.file = null;
+    this.isSubmitted = false;
+    this.messageService.add({ severity: 'info', summary: 'Archivo eliminado', detail: `El archivo ha sido eliminado.` });
+  }
+
+  onSubmit() {
+    if (this.file) {
+      this.isSubmitted = true;
+      setTimeout(() => {
+        const now = new Date();
+        const formattedDate = format(now, 'yyyy-MM-dd HH:mm:ss');
+        this.messageService.add({ severity: 'success', summary: 'Éxito', detail: `El archivo ${this.file!.name} ha sido subido con éxito a las ${formattedDate}.` });
+        setTimeout(() => {
+          this.messageService.add({ severity: 'info', summary: 'Revisión', detail: 'El archivo está en proceso de revisión.' });
+        }, 3000); // Simula un retraso en la revisión de 3 segundos
+      }, 0); // Mensaje de éxito inmediato al hacer clic en enviar
+    }
   }
 }
