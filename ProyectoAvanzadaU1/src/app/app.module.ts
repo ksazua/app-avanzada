5e80efa0055eb42bf7545a802c003a52c4515ca1
FabricioAlama2001 <88296370+FabricioAlama2001@users.noreply.github.com>
2024-06-18 19:02:54 -0500

    upload-file update

--- ProyectoAvanzadaU1/src/app/app.module.ts
+++ ProyectoAvanzadaU1/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { PetAdoptionComponent } from './pet-adoption/pet-adoption.component';
 import { TablaValidacionComprobanteComponent } from './tabla-validacion-comprobante/tabla-validacion-comprobante.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from "primeng/button";
-import {FormsModule} from "@angular/forms";
-import {ToolbarModule} from "primeng/toolbar";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ToolbarModule } from "primeng/toolbar";
 import { TablaValidaFormularioComponent } from './tabla-valida-formulario/tabla-valida-formulario.component';
-import {DialogModule} from "primeng/dialog";
+import { DialogModule } from "primeng/dialog";
 import { UploadFileComponent } from './upload-file/upload-file.component';
-
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,6 @@ import { UploadFileComponent } from './upload-file/upload-file.component';
     TablaValidacionComprobanteComponent,
     TablaValidaFormularioComponent,
     UploadFileComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -31,14 +33,17 @@ import { UploadFileComponent } from './upload-file/upload-file.component';
     ButtonModule,
     InputTextModule,
     TimelineModule,
-    ButtonModule,
     FormsModule,
+    ReactiveFormsModule,
     ToolbarModule,
-    DialogModule
-
+    DialogModule,
+    FileUploadModule,
+    ToastModule,
+    HttpClientModule  
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    MessageService
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
